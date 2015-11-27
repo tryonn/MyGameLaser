f1d359c4b6bbad4082228ece7bad1ac2c82e8e6f
Simão Menezes <simaomenezesneto@gmail.com>
2015-11-27 07:58:16 -0300

    destroy enemy.

---
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float health = 150;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Projectile laser = collider.gameObject.GetComponent<Projectile>();
+        if (laser)
+        {
+            health -= laser.GetDamage();
+            laser.Hit();
+
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
